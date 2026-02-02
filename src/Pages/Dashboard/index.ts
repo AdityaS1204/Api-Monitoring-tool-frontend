d78125af811c7de0ea42846abdf8d90fa7def22e
Aditya <adityas.singh.tech@gmail.com>
2026-02-02 15:36:20 +0530

    refactor: create barrel export for all dashboard related pages

---
+++ src/Pages/Dashboard/index.ts
@@ -0,0 +1,5 @@
+export { default as DashboardOverview } from './DashboardOverview';
+export { default as Analytics } from './Analytics';
+export { default as Activity } from './Activity';
+export { default as Reports } from './Reports';
+export { default as DashboardSettings } from './Settings';
