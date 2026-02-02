cf06b6343091b301b6fbaec9c672e6700501785d
Aditya <adityas.singh.tech@gmail.com>
2026-02-02 15:38:30 +0530

    refactor: export new dashboard compoments from global page index.

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -2,4 +2,14 @@ import Home from "./Home"
 import SignInPage from "./SignInPage"
 import SignUpPage from "./SignUpPage"
 import Docs from "./Docs"
+
+// Dashboard pages
+export {
+    DashboardOverview,
+    Analytics,
+    Activity,
+    Reports,
+    DashboardSettings
+} from "./Dashboard";
+
 export { Home, SignInPage, SignUpPage, Docs };
