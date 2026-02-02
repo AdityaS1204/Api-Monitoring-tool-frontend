0e36818ae815afab65c9514b4df653b094b09da8
Aditya <adityas.singh.tech@gmail.com>
2026-02-02 15:34:39 +0530

    feat: add DashboardOverview wrapper to integrate page content with main layout

---
+++ src/Pages/Dashboard/DashboardOverview.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '@/components/dashboard/DashboardLayout';
+import Overview from './Overview';
+
+export default function DashboardOverview() {
+    return (
+        <DashboardLayout>
+            <Overview />
+        </DashboardLayout>
+    );
+}
